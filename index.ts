93588d68d9d0da17555e4f5f68c028bc575b641a
quan <quan93909@gmail.com>
2025-08-31 20:22:55 +0700

    Tạo controller

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import sequelize from "./config/database";
 import Tour from "./models/tour.model";
+import clientRoutes from "./routes/client/index.route";
 
 dotenv.config();
 
@@ -13,15 +14,8 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/tours", async (req: Request, res: Response) => {
-  const tours = await Tour.findAll({
-    raw: true
-  });
-
-  res.render("client/pages/tours/index", {
-    tours: tours
-  });
-});
+// Client Routes
+clientRoutes(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
